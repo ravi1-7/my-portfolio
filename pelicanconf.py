ee8c68bf4ec15a01a626577d5e5c23be72ad939a
Ravichandra Parvatham <Ravi@MacBook-Pro-82.local>
2025-11-04 16:15:11 -0500

    added social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,17 +16,17 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+# LINKS = (
+#     ("Pelican", "https://getpelican.com/"),
+#     ("Python.org", "https://www.python.org/"),
+#     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+#     ("You can modify those links in your config file", "#"),
+# )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Linkedin", "https://www.linkedin.com/in/ravi-parvatham/"),
+    ("Github", "https://github.com/ravi1-7/"),
 )
 
 DEFAULT_PAGINATION = False
